c3d8b545de47d8b3190c24716336338a72cf1ee6
Nikolay Golubev <nik.v.golubev@gmail.com>
2025-03-10 19:56:28 -0700

    Initial population can be controlled

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(0, 0, 255)",  
+  "rgb(0, 150, 0)",
+  "rgb(255, 0, 255)"
+];
+
+document.getElementById("sD1").style.backgroundColor = colors[0];
+document.getElementById("sD2").style.backgroundColor = colors[1];
+document.getElementById("sD3").style.backgroundColor = colors[2];
+document.getElementById("sD4").style.backgroundColor = colors[3];
+
+//important, colors in the spectra appear in energy order
+var colors_energy = [
+  colors[2],
+  colors[3],
+  colors[0],
+  colors[1]
+];
+
 var slider = document.getElementById('slider');
 
 wI1F1 = (EF[0] - EI[0])*au2eV;
@@ -22,14 +42,20 @@ noUiSlider.create(slider, {
   orientation: 'vertical'
 });
 
-function compute_cs(t, w, D){
+var i = math.complex(0.,1.);
+
+function compute_cs(t, w, D, c1){
   var res = 0.;
+  var CI = [Math.sqrt(c1),Math.sqrt(1.-c1)];
 
   for(var I = 0; I < NI; I++){
+    var cI = CI[I];
     for(var J = 0; J < NI; J++){
-      var i = math.complex(0.,1.);
+      var cJ = CI[J];
+
       var phi = (EI[I] - EI[J]) * t;
-      var xiIJ = math.exp( math.multiply(i,phi) );
+      var cIJ = math.multiply(cI,cJ);
+      var xiIJ = math.multiply(cIJ, math.exp( math.multiply(i,phi) ));
 
       for(var F = 0; F < NF; F++){
         var dIF = D[I][F];
@@ -47,7 +73,7 @@ function compute_cs(t, w, D){
   return (4 * Math.PI * w / c) * res;
 }
 
-function compute_spec(D){
+function compute_spec(D,cI){
   //2d array of spectra
   var sigma = new Array(Nt);
   for(var it = 0; it < Nt; it++) {
@@ -55,7 +81,7 @@ function compute_spec(D){
     var t = tgrid[it];
     for(var iE = 0; iE < NE; iE++) {
       var w = Egrid[iE];
-      sigma[it][iE] = compute_cs(t,w,D);
+      sigma[it][iE] = compute_cs(t,w,D,cI);
     }
   }
 
@@ -65,7 +91,7 @@ function compute_spec(D){
   return sigma;
 }
 
-function compute_lineouts(D,energies){
+function compute_lineouts(D,cI,energies){
   var size = energies.length;
 
   var sigma = new Array(size);
@@ -74,16 +100,16 @@ function compute_lineouts(D,energies){
     var w = energies[indx];
     for(var it = 0; it < Nt; it++) {
       var t = tgrid[it];
-      sigma[indx][it] = compute_cs(t,w,D);
+      sigma[indx][it] = compute_cs(t,w,D,cI);
     }
   }
 
   //normalize to 1
   for(var indx = 0; indx < size; indx++) {
-    var sigma_min = Math.min(...sigma[indx].flat());
-    var sigma_max = Math.max(...sigma[indx].flat());
+    //var sigma_min = Math.min(...sigma[indx].flat());
+    //var sigma_max = Math.max(...sigma[indx].flat());
     for(var it = 0; it < Nt; it++) {
-      sigma[indx][it] = (sigma[indx][it] - sigma_min) / (sigma_max - sigma_min) - 0.5;
+      sigma[indx][it] = (sigma[indx][it] - global_sigma_min) / (global_sigma_max - global_sigma_min);
     }
   }
 
@@ -141,26 +167,35 @@ var lineout_layout = {
     title: {
       text: 'Time [fs]'
     }
-  } 
+  },
+
+  shapes: []
 };
 
-function get_dipoles() {
-  D1I1F = document.getElementById("sD1").value;
-  D1I2F = document.getElementById("sD2").value;
-  D2I1F = document.getElementById("sD3").value;
-  D2I2F = document.getElementById("sD4").value;
+// Appending money-formatting
+var Format = wNumb({
+	decimals: 1,
+});
 
-  document.getElementById("vD1").innerHTML = D1I1F;
-  document.getElementById("vD2").innerHTML = D1I2F;
-  document.getElementById("vD3").innerHTML = D2I1F;
-  document.getElementById("vD4").innerHTML = D2I2F;
+function get_parameters() {
+  var D1I1F = Format.from(document.getElementById("sD1").value);
+  var D1I2F = Format.from(document.getElementById("sD2").value);
+  var D2I1F = Format.from(document.getElementById("sD3").value);
+  var D2I2F = Format.from(document.getElementById("sD4").value);
+  var CI = Format.from(document.getElementById("sCI").value);
+
+  document.getElementById("vD1").innerHTML = Format.to(D1I1F);
+  document.getElementById("vD2").innerHTML = Format.to(D1I2F);
+  document.getElementById("vD3").innerHTML = Format.to(D2I1F);
+  document.getElementById("vD4").innerHTML = Format.to(D2I2F);
+  document.getElementById("vCI").innerHTML = Format.to(CI);
 
   var D = [
     [D1I1F, D1I2F],
     [D2I1F, D2I2F]
   ];
 
-  return D;
+  return [D,CI];
 }
 
 const zeros = (m, n) => [...Array(m)].map(e => Array(n).fill(0));
@@ -188,33 +223,37 @@ Plotly.newPlot('ATAS', data, layout);
 Plotly.newPlot('lineout', [], lineout_layout);
 
 function plot_ATAS() {
-  var D = get_dipoles();
-  var sigma = compute_spec(D);
+  var [D,cI] = get_parameters();
+  var sigma = compute_spec(D,cI);
   data[0].z = sigma;
   Plotly.react('ATAS', data, layout);
 };
 
 function plot_lineouts() {
-  var D = get_dipoles();
+  var [D,cI] = get_parameters();
 
   var lineout_energies_eV = slider.noUiSlider.get(true);
   var lineout_energies = lineout_energies_eV.map(function(E){ return E/au2eV; });
   var nlineouts = lineout_energies.length;
 
-  var lineouts = compute_lineouts(D,lineout_energies);
+  var lineouts = compute_lineouts(D,cI,lineout_energies);
 
   var traces = [];
   var shapes = [];
   for(var indx = 0; indx<nlineouts; indx++){
     var w = lineout_energies_eV[indx];
-    var lineout = lineouts[indx].map(function(v){ return 0.05*v + w; });
+
+    var min = Math.min(...lineouts[indx].flat());
+    var max = Math.max(...lineouts[indx].flat());
+    var shift = 0.5 * (max - min) + min;
+    var lineout = lineouts[indx].map(function(v){ return 0.05*(v-shift) + w; });
 
     var trace = {
       x: tgrid_fs,
       y: lineout,
       mode: 'lines',
       line: {
-        color: colors[indx],
+        color: colors_energy[indx],
         width: 3
       }        
     };
@@ -227,7 +266,7 @@ function plot_lineouts() {
       x1: 100,
       y1: lineout_energies_eV[indx],
       line:{
-        color: colors[indx],
+        color: colors_energy[indx],
         width: 4,
       }
     };
@@ -235,6 +274,7 @@ function plot_lineouts() {
   }
 
   layout.shapes = shapes;
+  //lineout_layout.shapes = shapes;
 
   Plotly.relayout('ATAS', layout);
   Plotly.react('lineout', traces, lineout_layout);
